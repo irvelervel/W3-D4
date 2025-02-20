bdf769509908ad514d85b7b56aa9f96de81125d5
Stefano Casasola <ste.casasola@gmail.com>
2025-02-20 11:02:40 +0100

    Creates a working table of days

--- js/script.js
+++ js/script.js
@@ -40,4 +40,63 @@ const showMonthInHeader = function () {
   h1Title.innerText = currentMonth
 }
 
-showMonthInHeader()
+// vado a capire quanti giorni ha il mese corrente
+const daysInThisMonth = function () {
+  // il ragionamento è: dato il giorno corrente, vado a ricercare il primo giorno
+  // del mese successivo. Da questa data che ottengo, sottraggo 1 giorno. Ottengo
+  // quindi l'ultimo giorno del mese corrente, il cui numero rappresenta anche
+  // il numero TOTALE dei giorni del mese corrente.
+
+  // calcoliamo ora la data per trovare il primo giorno del prossimo mese
+  // in che anno siamo
+  const year = now.getFullYear() // 2025
+  const month = now.getMonth() // 1
+
+  // a questo punto, utilizzo queste informazioni per generare una NUOVA DATA
+  // che rappresenterà il PRIMO GIORNO del mese successivo:
+  const lastDayOfCurrentMonth = new Date(year, month + 1, 0) // "lo 0 di marzo" (il 28 febbraio)
+  // questa variabile che ho appena trovato rappresenta la data dell'ultimo giorno del mese corrente
+  // da questa data io estraggo il numero del giorno
+  console.log('ULTIMO GIORNO DEL MESE', lastDayOfCurrentMonth)
+  const numberOfDays = lastDayOfCurrentMonth.getDate() // 28
+  console.log('da questa data estrapoliamo il numero del giorno', numberOfDays)
+  return numberOfDays
+}
+
+const createCalendarCells = function (numberOfDays) {
+  // questa funzione si occupa di creare da zero le celle per il calendario
+  // e di appenderle nel DOM
+
+  // numberOfDays rappresenta il NUMERO di celle che devono venire create
+  // devo eseguire la creazione di UNA cella NUMBEROFDAYS volte!
+
+  for (let i = 0; i < numberOfDays; i++) {
+    // in febbraio, questo ciclo viene eseguito 28 volte!
+
+    // creiamone una!
+    const cell = document.createElement('div') // <div></div>
+    cell.classList.add('day') // <div class="day"></div>
+
+    // contenuto di cell
+    const cellValue = document.createElement('h3')
+    cellValue.innerText = i + 1
+
+    // appendo il contenuto della cella alla cella
+    cell.appendChild(cellValue) // <div class="day"> <h3>1</h3> </div>
+
+    // trovo un riferimento alla section con id="calendar"
+    const calendarSection = document.getElementById('calendar')
+    calendarSection.appendChild(cell) // appendo la singola cella alla section calendario
+  }
+}
+
+// da qui cominciano le INVOCAZIONI delle mie funzioni
+showMonthInHeader() // metto il nome del mese nell'h1
+
+// popoliamo il calendario
+
+// 1) capire quante celle bisogna creare
+const numberOfDays = daysInThisMonth() // calcolare il numero dei giorni del mese
+// numberOfDays è 28!
+// 2) per tot volte (28), creo una cella
+createCalendarCells(numberOfDays)
