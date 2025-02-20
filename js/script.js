2ef217793f64a4f44882c10f8a0d84e9bf3df00e
Stefano Casasola <ste.casasola@gmail.com>
2025-02-20 12:07:33 +0100

    Let's talk about events saving

--- js/script.js
+++ js/script.js
@@ -24,6 +24,23 @@ const monthNames = [
   'Dicembre', // 11
 ]
 
+// oggi siamo nel mese di febbraio
+// febbraio ha 28 giorni
+// per ognuno di questi 28 giorni, potremo salvare un numero INFINITO di appuntamenti
+// un evento voglio salvarlo così: ["18:00 - Dentista", "13:00 - Pastasciutta", etc.]
+
+// questa è la nostra struttura dati per salvare infiniti appuntamenti
+// nel nostro calendario
+// un array contentente n array, dove n è il numero dei giorni del mese
+// [
+//   [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+//   [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+//   [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
+//   [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+// ]
+
+const memory = []
+
 const showMonthInHeader = function () {
   // dobbiamo riempire quell'<h1> lasciato volutamente vuoto in HTML
   // solo JS può sapere con che parola riempirlo!
@@ -73,13 +90,59 @@ const createCalendarCells = function (numberOfDays) {
   for (let i = 0; i < numberOfDays; i++) {
     // in febbraio, questo ciclo viene eseguito 28 volte!
 
+    // aggiungo alla memoria del calendario una "sottosezione" per questa giornata
+    memory.push([])
+
     // creiamone una!
     const cell = document.createElement('div') // <div></div>
     cell.classList.add('day') // <div class="day"></div>
 
+    // rendiamo cliccabile la cella appena creata!
+    cell.addEventListener('click', () => {
+      // questo succederà al click di una qualsiasi cella
+
+      // al click di una cella, devo prima di tutto deselezionare l'ultima cella
+      // cliccata, se esiste
+      //   const alreadySelected = document.querySelector('.selected')
+      const alreadySelected = document.querySelector('.selected')
+      // a questo punto alreadySelected può essere NULL o un'altra cella precedentemente
+      // cliccata
+      if (alreadySelected !== null) {
+        // vado a togliere la classe "selected" dalla cella alreadySelected
+        alreadySelected.classList.remove('selected')
+      }
+
+      //  versione con getElementsByClassName
+      //   const alreadySelected = document.getElementsByClassName('selected')
+      //   // a questo punto alreadySelected può essere NULL o un'altra cella precedentemente
+      //   // cliccata
+      //   console.log('alreadySelected', alreadySelected)
+      //   if (alreadySelected.length > 0) {
+      //     // vado a togliere la classe "selected" dalla cella alreadySelected
+      //     alreadySelected[0].classList.remove('selected')
+      //   }
+
+      // questa riga aggiunge la classe "selected" alla cella cliccata
+      cell.classList.add('selected') // inserire il nome della classe
+
+      // oltre a colorare la cella, dobbiamo trasportarne il valore in basso a sx
+      //   nella sezione che ho chiamato "newMeetingDay"
+      const spanToReplace = document.getElementById('newMeetingDay')
+      spanToReplace.innerText = i + 1
+      spanToReplace.classList.add('hasDay')
+    })
+
     // contenuto di cell
     const cellValue = document.createElement('h3')
     cellValue.innerText = i + 1
+
+    // calcoliamoci qual è il giorno di oggi
+    // solo ad UNA cella verrà assegnata la classe "color-epic"
+    const today = now.getDate() // 20
+    if (i + 1 === today) {
+      // se il numero della cella è 20...
+      cellValue.classList.add('color-epic')
+    }
 
     // appendo il contenuto della cella alla cella
     cell.appendChild(cellValue) // <div class="day"> <h3>1</h3> </div>
@@ -88,6 +151,10 @@ const createCalendarCells = function (numberOfDays) {
     const calendarSection = document.getElementById('calendar')
     calendarSection.appendChild(cell) // appendo la singola cella alla section calendario
   }
+
+  // finito il for
+  // vado a vedere com'è fatto memory
+  console.log('MEMORY', memory)
 }
 
 // da qui cominciano le INVOCAZIONI delle mie funzioni
@@ -100,3 +167,21 @@ const numberOfDays = daysInThisMonth() // calcolare il numero dei giorni del mes
 // numberOfDays è 28!
 // 2) per tot volte (28), creo una cella
 createCalendarCells(numberOfDays)
+
+// gestiamo la logica del form per SALVARE gli appuntamenti
+const meetingForm = document.getElementById('newMeetingForm')
+meetingForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log('SALVIAMO APPUNTAMENTO')
+
+  // su quale giorno ho cliccato? lo prendo dallo span in basso a sx
+  const spanToReplace = document.getElementById('newMeetingDay') // lo span
+  const selectedDay = parseInt(spanToReplace.innerText) // il valore dello span
+  // '22' -> 22
+  const timeInput = document.getElementById('newMeetingTime') // <input />
+  const selectedTime = timeInput.value
+  const meetingInput = document.getElementById('newMeetingName')
+  const selectedName = meetingInput.value
+
+  const appointment = `${selectedTime} - ${selectedName}` // "18:00 - Dentista"
+})
